67be0cbecd06b3847395bc5d1d4b4e222fb7da54
Ayan <44862892+ayan8125@users.noreply.github.com>
2020-09-01 23:45:26 +0530

    Update settings.py

--- pubgtournament/settings.py
+++ pubgtournament/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4^!_s3&!ezst9vg09@g2oo$4*uz4w^twzqe^$f*qp4iocn4j_j'
+SECRET_KEY = '**********'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
